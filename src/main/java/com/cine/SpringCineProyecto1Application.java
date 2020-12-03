863f3ca674013c992bf377f9453993be02b584bf
Isaac <Isaac@DESKTOP-N999OAF>
2020-12-02 23:58:09 -0300

    update

---
+++ src/main/java/com/cine/SpringCineProyecto1Application.java
@@ -0,0 +1,13 @@
+package com.cine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCineProyecto1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCineProyecto1Application.class, args);
+	}
+
+}
